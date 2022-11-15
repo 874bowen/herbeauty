d2c727756f55709c46a594404a0fc80d71182205
Ivan Bowen <bowenivan16@gmail.com>
2022-11-15 07:02:30 +0300

    add nav and featured component

--- components/Featured.js
+++ components/Featured.js
@@ -4,28 +4,60 @@ import React from 'react';
 import Image from "next/image";
 import { useSession, signIn, signOut } from 'next-auth/react';
 
+
+
 const Featured = ({ images }) => {
    console.log(images);
-   const {data: session} = useSession();
-   
-   const handleAddToCart = () => {
-      console.log("Logged in");
-      // else signIn;
+
+   const { data: session } = useSession();
+
+   const handleAddToCart = (email, product_name) => {
+      
+      fetch("/api/add-to-cart", {
+         method : "POST",
+         headers : {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify({
+            email: email, 
+            name: product_name
+         }),
+      }).then(() => window.location.reload());
    }
+
    return (
       <div className="w-full md:h-screen p-2 flex items-center py-16">
-         <div className="mt-5 max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+         <div className="mt-5 max-w-[1240px] m-auto grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-8">
             {images.map(image => {
+               let [name, price] = image.title.split("_");
                return (
-                  <div  key={image.id} className=" relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4">
-                     <Image  width={250} height={250}  src={image.image} className="rounded-xl group-hover:opacity-20" alt="/" />
-                     <div className="absolute bottom-[12%] right-[23%] translate-x-[50%] translate-y-[50%]">
+
+                  <div key={image.id} className=" relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-sm md:rounded-lg p-2 md:p-3 hover:scale-105 ease-in duration-300">
+                     <div>
+                        <div>
+                        <Image width={200} height={200} src={image.image} className=" group-hover:opacity-20" alt="/" />
                      
-                     <Link href="" onClick={ session ? handleAddToCart : signIn}>
-                        <p className="text-center rounded-lg bg-[#292a5e] text-[#efd4e7] font-bold lg:text-md text-sm cursor-pointer p-2">Add To Cart</p>
-                     </Link> 
-                     </div> 
+                        <div className="absolute bottom-[12%] right-[23%] translate-x-[50%] translate-y-[50%]">
+
+                           {/* <Link href="" onClick={session ? handleAddToCart : signIn}>
+                              <p className="text-center rounded-lg bg-[#292a5e] text-[#efd4e7] font-bold lg:text-md text-sm cursor-pointer p-2">Add To Cart</p>
+                           </Link> */}
+                        </div>
+                        <div className="absolute top-[-2%] left-[-10%] translate-x-[50%] translate-y-[50%]">
+                           <p className="text-center rounded-lg bg-[#efdded] text-[#292a5e] font-bold lg:text-lg text-md cursor-pointer p-2">${price}</p>
+                        </div>
+                        </div>
+                        <div className='bg-[#29215e] text-center text-md w-full pt-2 pb-2 uppercase tracking-wide text-[#efdded]'>
+                           <Link href="" onClick={(e) => {
+                              e.preventDefault();
+                              handleAddToCart(session.user.email, image.title);
+                              }}>Add to Cart</Link>
+                        </div>
+
+                     </div>
                   </div>
+                  
+
                   // <li key={image.id}>
                   //    <a href={image.link} rel="noreferrer">
                   //       <div className='w-full h-auto m-auto shadow-xl shadow-[#424369] roundex-xl flex items-center p-4 justify-center hover:scale-105 ease-in duration-300'>
@@ -44,3 +76,5 @@ const Featured = ({ images }) => {
 }
 
 export default Featured;
+
+
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,16 +3,35 @@ import Link from 'next/link';
 import Image from "next/image";
 import logo from "../public/assets/hb_logo.png";
 import { useSession, signIn, signOut } from 'next-auth/react';
-import leftNav from "./LeftNav";
 
 const Navbar = () => {
-	const [nav, setNav] = useState(false);
+
 	const { data: session } = useSession();
-	const handleNav = () => {
-		setNav(!nav);
+	let name = "";
+	let email = "";
+	if (session){
+		email = session.user.email
+		name = session.user.name.split(" ")[0]
 	}
+	let [number, setNumber] = useState(1);
+	(async function getCart () {   
+      const cartItems = await fetch("/api/get-carts", {
+         method: "POST",
+         headers: {
+           "Content-Type": "application/json"
+         },
+			body: JSON.stringify({
+				email: email
+			})
+       }).then(r => r.json());
+      
+       setNumber(cartItems.length);
+   })()
+
+
+
 	
-	
+
 	return (
 		<div className="fixed w-full h-20 shadow-xl z-[100]">
 			<div className="flex justify-between bg-[#292a5e] text-[#efd4e7]  items-center w-full h-full px-2 2xl:px-16">
@@ -23,21 +42,33 @@ const Navbar = () => {
 					height="100"
 				/>
 				<div>
-					<ul className="hidden items-center md:flex">
+					<ul className="items-center gap-3 flex">
+
 						<Link href="/">
-							<li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+							<li className="ml-10 text-sm uppercase hover:border-b">
+								{(session) && <p className="hidden md:block mr-6">Welcome, {name}!</p>}
+							</li>
 						</Link>
-						<Link href="/">
-							<li className="ml-10 text-sm uppercase hover:border-b">About</li>
+						{(session) &&
+						<>
+						<Link href="/" onClick={signIn}>
+							<button>Cart {number}</button>
+						
 						</Link>
-						<Link href="/" onClick={signOut}>{(session) && 
+						<Link href="/" onClick={signOut}>
 							<Image
 								src={session.user.image}
 								alt="/"
 								width="30"
 								height="30"
-								className="rounded-xl group-hover:opacity-20"
+								className="rounded-xl ml-3 group-hover:opacity-20"
 							/>
+						</Link>
+						</>
+						}
+						
+						<Link href="/" onClick={signIn}>{(!session) &&
+							<button>Login</button>
 						}
 						</Link>
 					</ul>
